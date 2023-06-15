fb05031cc7eaba2db087b154accf1007702ac6f6
Juankzx <juancarloszg915@gmail.com>
2023-06-15 18:54:56 -0400

    primer commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <AdminTemplate>
+    <div slot="body">
+      h3
+    </div>
+  </AdminTemplate>  
+</template>
+
+<script>
+import AdminTemplate from '../components/AdminTemplate.vue';
+
+export default {
+    name: "IndexPage",
+    head() {
+        return {
+            title: "Index",
+        };
+        ;
+    },
+    components: { AdminTemplate }
+};
+</script>
